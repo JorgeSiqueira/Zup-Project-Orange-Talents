dbd727fe63257303c9a28b00e6f25c4260847101
JorgeSiqueira <jorgetsiqueira@gmail.com>
2021-01-06 12:21:27 -0200

    commit inicial

---
+++ src/main/java/com/jorgesiqueira/Zup/Project/Orange/Talents/ZupProjectOrangeTalentsApplication.java
@@ -0,0 +1,13 @@
+package com.jorgesiqueira.Zup.Project.Orange.Talents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZupProjectOrangeTalentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZupProjectOrangeTalentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jorgesiqueira/Zup/Project/Orange/Talents/ZupProjectOrangeTalentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgesiqueira.Zup.Project.Orange.Talents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZupProjectOrangeTalentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
